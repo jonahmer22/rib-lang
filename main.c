7adc16b6be0719fcfa6fe89844796ebcfbb8eedc
jonahmer22 <jonah@jonahmerriam.net>
2026-04-20 17:45:05 -0400

    Initial working example
    example usage of embedding Cortex-VM set up as an execution target

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "cortex-vm.h"
 
-int main(void){
-	return 0;
+int main(void) {
+    const char *src =
+        "main:\n"
+        "    addi a0, zero, msg\n"
+        "    addi a13, zero, 5\n"   // SYS_PRINT_STR
+        "    syscall\n"
+        "    addi a0, zero, 0\n"
+        "    addi a13, zero, 0\n"   // SYS_EXIT
+        "    syscall\n"
+        ".data\n"
+        "    msg: \"Hello from embedded Cortex-VM!\\n\"\n";
+
+    return cortexExecSource(src);
 }
