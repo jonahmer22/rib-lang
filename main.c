36c8fccc8e02242c666f8d0d90bf069b08b2e41d
jonahmer22 <jonah@jonahmerriam.net>
2026-05-10 22:40:44 -0400

    Updated minimal example
    Updated minimal example to include exit codes

--- main.c
+++ main.c
@@ -6,9 +6,12 @@
 // Header includes
 
 int main(){
+    // minimal example that just prints 5 and exits with that value
     const char *src=
         "addi a0, zero, 5"
         "addi a13, zero, 1"
+        "syscall"
+        "addi a13, zero, 0"
         "syscall";
 
     return cortexExecSource(src);
