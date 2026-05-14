155a8164320fbb8779a88eff00cafb777ff91f56
jonahmer22 <jonah@jonahmerriam.net>
2026-05-13 22:03:56 -0400

    Updated example to verify persistent runtime

--- main.c
+++ main.c
@@ -10,11 +10,17 @@ int main(){
 
     // minimal example that just prints 5 and exits with that value
     const char *src=
-        "addi a0, zero, 5"
+        "addi a0, a0, 5"
         "addi a13, zero, 1"
         "syscall"
         "addi a13, zero, 0"
         "syscall";
 
-    return cortexVMExecSource(vm, src);
+    cortexVMExecSource(vm, src);
+
+    int exit_c = cortexVMExecSource(vm, src);
+
+    cortexVMDestroy(vm);
+
+    return exit_c;
 }
